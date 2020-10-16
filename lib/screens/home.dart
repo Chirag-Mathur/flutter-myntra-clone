9fb0bbd0f5ea07e6e9b122715ccfb6975a650da6
Amit Bhave <amitbhave10@gmail.com>
2020-10-16 14:55:45 +0530

    Login, otp complete

---
+++ lib/screens/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  static const routeName = '/home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Home screen'),
+    );
+  }
+
+}
---
+++ lib/utils/asset_constants.dart
@@ -0,0 +1,3 @@
+const base_url = 'https://constant.myntassets.com/pwa/assets/img';
+const banner_login_landing = '$base_url/banner_login_landing_500.jpg';
+const mobile_verification = '$base_url/3a438cb4-c9bf-4316-b60c-c63e40a1a96d1548071106233-mobile-verification.jpg';
